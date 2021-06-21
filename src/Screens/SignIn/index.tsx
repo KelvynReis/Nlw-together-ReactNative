3f4e9fb36f0afdde5065be76a5704dacce0daa90
Kelvyn Reis <kelvyn2reis@gmail.com>
2021-06-21 17:08:17 -0300

    splash Screen

--- src/Screens/SignIn/index.tsx
+++ src/Screens/SignIn/index.tsx
@@ -1,13 +1,47 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { 
+    View,
+    Text,
+    Image,
+    StatusBar,
+} from 'react-native';
 
+import { ButtonIcon } from '../../components/ButtonIcon';
+import IllustrationImg from '../../assets/illustration.png'
 import { styles } from './styles'
 
 
 export function SignIn(){
     return (
         <View style={styles.container}>
-            <Text> Hello Besto Friendo </Text>
+            <StatusBar
+                barStyle="light-content"
+                backgroundColor="transparent"
+                translucent
+            />
+
+            <Image
+                source={IllustrationImg}
+                style={styles.image}
+                resizeMode="stretch"
+            />
+
+            <View style={styles.content}>
+                <Text style={styles.title}>
+                    Organize{`\n`}
+                    suas jogatinas{`\n`}
+                    facilmente
+                </Text>
+
+                <Text style={styles.subtitle}>
+                    Crie grupos para jogar seus games {`\n`}
+                    favoritos com seus amigos
+                </Text>
+                <ButtonIcon
+                 title="Entrar com Discord"
+                 activeOpacity={0.7}
+                 />
+            </View>
         </View>
     );
 }
--- src/Screens/SignIn/styles.ts
+++ src/Screens/SignIn/styles.ts
@@ -1,9 +1,32 @@
 import { StyleSheet } from "react-native";
 
+import { theme } from "../../global/styles/theme";
+
 export const styles = StyleSheet.create({
     container:{
         flex: 1, //ocupar toda a tela
         justifyContent: 'center', // alinhar na vertical
-        alignItems: 'center', //alinhar na horizontal
+        alignItems: 'center',
+        backgroundColor: theme.colors.background //alinhar na horizontal
+    },
+    image: {
+        width: '100%',
+        height: 360,
+    },
+    content:{
+        marginTop: -40,
+        paddingHorizontal: 50,
+    },
+    title: {
+        color: theme.colors.heading,
+        textAlign: 'center',
+        fontSize: 40,
+        marginBottom: 16,
+    },
+    subtitle:{
+        color: theme.colors.heading,
+        fontSize: 15,
+        textAlign: 'center',
+        marginBottom: 64,
     }
 });
